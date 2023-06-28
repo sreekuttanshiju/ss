2a05386e40c7724cf831c58188180807a41f9efb
sreekuttanshiju <sreekuttanps225@gmail.com>
2023-06-29 00:16:51 +0530

    first commit

--- mainn.js
+++ mainn.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["photographer","designer","webdeveloper"],
+    strings: ["PHOTOGRAPHER","VIDEOGRAPHER","EDITOR"],
     typeSpeed: 60,
     backSpeed: 60,
     backDelay: 500,
